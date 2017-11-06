86cf523e42649bec90009e4b8de36170367122dc
dogwood008 <dogwood008+github@gmail.com>
2017-11-07 00:45:53 +0900

    :+1: modify log output
    (append thread id in CRC16)

--- Keras-RL_DQN_FX.py
+++ Keras-RL_DQN_FX.py
@@ -11,8 +11,11 @@ mpl.use('tkagg')
 import numpy as np
 import pandas as pd
 import talib
-from logging import getLogger, DEBUG, INFO, WARN
+from logging import getLogger, DEBUG, INFO, WARN, ERROR, CRITICAL
 import os
+import logging
+from logging import StreamHandler, LogRecord
+import base64
 
 from hist_data import HistData
 from fx_trade import FXTrade
@@ -23,40 +26,63 @@ from debug_tools import DebugTools
 # In[ ]:
 
 
-IS_SEND_LOG_TO_STACKDRIVER = True
-if IS_SEND_LOG_TO_STACKDRIVER:
-    import logging
-    import google.cloud.logging
-    from google.cloud.logging.handlers import CloudLoggingHandler
-    jupyter_logger = logging.getLogger()
-    jupyter_logger.setLevel(logging.WARN)
-    client = google.cloud.logging.Client         .from_service_account_json(os.environ.get('GOOGLE_SERVICE_ACCOUNT_JSON_PATH'))
-    client.setup_logging(log_level=logging.INFO)
-    handler = CloudLoggingHandler(client, name='deepfx')
-    logger = logging.getLogger()
-else:
-    from logging import StreamHandler
-    logger = getLogger(__name__)
-    handler = StreamHandler()
-    logger.setLevel(INFO)
-logger.addHandler(handler)
-logger.info('DeepFX Started: %s' % DebugTools.now_str())
+import crcmod
+class LogRecordWithCRC16ThereadID(logging.LogRecord):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.crc16threadid = self._calc_crc16(self.process)
+
+    def _calc_crc16(self, int_value):
+        byte_array = str(int_value).encode('utf-8')
+        crc16 = crcmod.Crc(0x18005)
+        crc16.update(byte_array)
+        return crc16.hexdigest()
+
+def init_logger(sd_loglevel=logging.WARN, stream_loglevel=logging.CRITICAL):
+    logging.setLogRecordFactory(LogRecordWithCRC16ThereadID)
+    logger = logging.getLogger('deepfx')
+    logger.setLevel(sd_loglevel)
+    formatter = logging.Formatter('[%(crc16threadid)s] %(message)s')
+
+    if sd_loglevel:
+        import google
+        from google.cloud.logging import Client
+        from google.cloud.logging.handlers import CloudLoggingHandler
+        client = google.cloud.logging.Client             .from_service_account_json(os.environ.get('GOOGLE_SERVICE_ACCOUNT_JSON_PATH'))
+        handler = CloudLoggingHandler(client, name='deepfx')
+        handler.setLevel(sd_loglevel)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        handler = None
+
+    if stream_loglevel:
+        handler = StreamHandler()
+        handler.setLevel(stream_loglevel)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        handler = None
+
+    return logger
 
 
 # In[ ]:
 
 
-#import imp
-#import sys
-#del(hist_data)
-#from hist_data import HistData
-#del(hist_data)
-#imp.reload(hist_data)
-#imp.reload(sys.modules[hist_data.__module__])
+deepfx_logger = init_logger()
+deepfx_logger.critical('DeepFX Started: %s' % DebugTools.now_str())
+deepfx_logger.debug   ('loglevel debug    test')
+deepfx_logger.info    ('loglevel info     test')
+deepfx_logger.warning ('loglevel warn     test')
+deepfx_logger.error   ('loglevel error    test')
+deepfx_logger.critical('loglevel critical test')
+
+
+# In[ ]:
+
+
 hd = HistData(csv_path = 'historical_data/DAT_ASCII_USDJPY_M1_201710_m5.csv',
                      begin_date='2017-10-02T00:00:00',
-                     end_date='2017-10-02T23:59:59')
-                     #end_date='2017-10-09T23:59:59')
+                     end_date='2017-10-09T23:59:59')
 
 
 # In[ ]:
@@ -72,7 +98,7 @@ hd.data()
 env = FXTrade(1000000, 0.08, hd, logger=logger)
 #env = FXTrade(1000000, 0.08, h, logger=logger)
 prepared_model_filename = None #'Keras-RL_DQN_FX_model_meanq1.440944e+06_episode00003.h5'
-dfx = DeepFX(env, prepared_model_filename=prepared_model_filename, steps = 100000, logger=logger)
+dfx = DeepFX(env, prepared_model_filename=prepared_model_filename, steps = 100000, logger=deepfx_logger)
 
 
 # In[ ]:
@@ -83,6 +109,12 @@ if is_to_train:
     dfx.train(is_for_time_measurement=True)
 else:
     dfx.test(1, [EpisodeLogger()])
+
+
+# In[ ]:
+
+
+deepfx_logger.critical('DeepFX Finished: %s' % DebugTools.now_str())
 
 
 # In[ ]:
@@ -103,12 +135,6 @@ upper, middle, lower = talib.BBANDS(data.values, timeperiod=20, matype=talib.MA_
 
 
 data.values
-
-
-# In[ ]:
-
-
-logger.info('DeepFX Finished: %s' % DebugTools.now_str())
 
 
 # ## References
