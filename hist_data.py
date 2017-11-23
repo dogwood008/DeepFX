d696784af02063891a50a0ae4b58057bfbdca5b8
dogwood008 <dogwood008+github@gmail.com>
2017-11-24 00:58:01 +0900

    :+1: adopt for bitcoin
    https://www.kaggle.com/mczielinski/bitcoin-historical-data/data

--- hist_data.py
+++ hist_data.py
@@ -11,7 +11,7 @@ import pandas as pd
 
 
 class HistData:
-    def __init__(self, csv_path=None, begin_date=None, end_date=None, range_limited=True):
+    def __init__(self, csv_path=None, begin_date=None, end_date=None, range_limited=True, sep=';'):
         self.csv_path = csv_path
         with open(self.csv_path) as f:
             try:
@@ -27,7 +27,7 @@ class HistData:
                                     names=names, header=header,
                                     index_col='Date', parse_dates=['Date'],
                                     dtype={'Date': 'str', 'Open': 'float', 'High': 'float', 'Low': 'float', 'Close': 'float', 'Volume': 'int'},
-                                    sep=';')
+                                    sep=sep)
         self.begin_date = begin_date
         self.end_date = end_date
         if range_limited:
@@ -64,12 +64,46 @@ class HistData:
 # In[ ]:
 
 
+class BitcoinHistData(HistData):        
+    # https://www.kaggle.com/mczielinski/bitcoin-historical-data/data
+    # coincheckJPY_1-min_data_2014-10-31_to_2017-10-20.csv
+    import datetime
+    def __init__(self, csv_path=None, begin_date=None, end_date=None, range_limited=True, sep=','):
+        self.csv_path = csv_path
+        with open(self.csv_path) as f:
+            try:
+                int(f.readline()[0])
+                print('no header')
+                header = None
+                names = ('Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume_(BTC)')
+            except:
+                print('header is included')
+                header = 0
+                names = None
+        self.csv_data = pd.read_csv(self.csv_path,
+                                    names=names, header=header,
+                                    parse_dates=['Timestamp'],
+                                    dtype={'Timestamp': 'int', 'Open': 'float', 'High': 'float', 'Low': 'float', 'Close': 'float', 'Volume': 'int'},
+                                    sep=sep)
+        self.csv_data['Date'] =  pd.to_datetime(self.csv_data['Timestamp'], unit='s')
+        self.csv_data.index = self.csv_data['Date']
+        self.begin_date = begin_date
+        self.end_date = end_date
+        if range_limited:
+            self.csv_data = self.data()
+            if(len(self.csv_data) is 0):
+                raise ValueError('Given csv in the range was empty.')
+
+
+# In[ ]:
+
+
 if __name__ == '__main__':
     import numpy as np
     begin = '2017-10-01T00:00:00'
     end = '2017-10-07T23:59:59'
     
-    hd = HistData(csv_path = 'historical_data/DAT_ASCII_USDJPY_M1_201710_m5.csv',
+    hd = BitcoinHistData(csv_path = 'historical_data/coincheckJPY_1-min_data_2014-10-31_to_2017-10-20.csv',
                      begin_date=begin,
                      end_date=end)
     #print(hd.data())
@@ -78,10 +112,4 @@ if __name__ == '__main__':
     print(hd.date_at(1))
     print(hd.close_at(1))
     print(hd.values_at(1))
-
-
-# In[ ]:
-
-
-
 
