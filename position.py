e36a63fbc2e5321376b0d3b6b774e64901692eae
dogwood008 <dogwood008+github@gmail.com>
2017-11-28 00:36:48 +0900

    :+1: use estimated value

--- position.py
+++ position.py
@@ -17,14 +17,7 @@ class Position:
         self.amount = amount
         self.buy_or_sell = buy_or_sell
     
-    ''' 総利益を計算する '''
-    def calc_profit_by(self, now_price):
-        return self._calc_unit_profit_by(now_price) * self.amount
-
-    ''' 単位あたりの利益を計算する '''
-    def _calc_unit_profit_by(self, now_price):
-        if self.buy_or_sell == 'buy' or self.buy_or_sell == Action.BUY.value:
-            return now_price - self.price
-        else:
-            return self.price - now_price
+    ''' 評価額を計算する '''
+    def estimated_value(self, now_price):
+        return now_price * self.amount
 
